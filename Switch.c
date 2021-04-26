87e7bcc5ec9e958d6b359c4216c594a7841963dc
chloetang28 <chloeatang@utexas.edu>
2021-04-26 00:23:50 -0500

    Update Switch.c

--- Switch.c
+++ Switch.c
@@ -13,3 +13,26 @@
 // Code files contain the actual implemenation for public functions
 // this file also contains an private functions and private data
 
+
+void PortE_Init(void){
+	SYSCTL_RCGCGPIO_R |= 0x10; // Port E
+	
+	__asm__ {
+		NOP
+		NOP
+	}
+	
+	GPIO_PORTE_DIR_R &= ~(0x03); //PE0-1 = inputs
+	GPIO_PORTE_DEN_R |= 0x03;
+	
+	GPIO_PORTE_PDR_R |= 0x03; // pull down for PE0-1
+
+}
+
+uint32_t PortE_In(void){ 
+  // write this
+	uint8_t SwitIn = GPIO_PORTE_DATA_R;
+	SwitIn &= 0x03; // mask off bits used
+	return SwitIn;
+}
+
