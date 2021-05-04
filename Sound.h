655eb39f20dda3c1cae6e3a350d5ba2234a9e7cd
chloetang28 <chloeatang@utexas.edu>
2021-05-03 21:28:24 -0500

    Update Sound.h

--- Sound.h
+++ Sound.h
@@ -13,7 +13,7 @@
 
 extern const uint8_t explosion[]; 
 
-typedef enum {Engine, Explosion, BoomSound, FLaunch, Alarm} soundEffect; 
+typedef enum {Engine, Explosion, BoomSound, FLaunch, Alarm, Win} soundEffect; 
 
 extern uint32_t pIndex;
 void playSample(void);
